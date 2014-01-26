f38e5b22f5a0e1e901f32f3f28d693b96c20df9d
Andrew J Wood <andywood@ruby.cat.pdx.edu>
2014-01-26 08:49:05 -0800

    Fixed parsing of strings >2 words in length

--- magicbot.sh
+++ magicbot.sh
@@ -12,7 +12,7 @@ CHAN=#oracle
 tail -f .botfile | openssl s_client -connect irc.cat.pdx.edu:6697 | while true; do
     if [[ -z $STARTED ]]; then
         echo "USER oracle oracle oracle :oracle" >> .botfile
-        echo "NICK oracle" >> .botfile
+        echo "NICK magicbot" >> .botfile
         echo "JOIN $CHAN" >> .botfile
         STARTED="yes"
     fi
@@ -27,17 +27,23 @@ tail -f .botfile | openssl s_client -connect irc.cat.pdx.edu:6697 | while true; 
 
         # Fetch card name from IRC query string and store capitalized and non-capitalized versions of it as variables.
         CARDNAME=`echo -n $IRC | sed 's/.*!card //' | tr '[:upper:]' '[:lower:]' | tr -dc '[:print:]'`
-        CARDNAME_CAP="`echo "${CARDNAME^}" | sed 's: .:\U&:g'`"
-echo "$CARDNAME_CAP"
+        CARDNAME_CAP="`echo -n "${CARDNAME^}" | sed 's: .:\U&:g' `"
+        CARDNAME_CAP="`echo -n "$CARDNAME_CAP" | sed 's:Of:of:g' | sed 's:The:the:'`"
+echo "$CARDNAME_CAP" | xxd
 
         # Fetch raw data from magiccards.info with curl, which we can then pare down with sed parsing to extract the target card info.
         MTGI_RAW=$(curl -s "http://magiccards.info/query?q=`echo -n $CARDNAME | sed 's: :+:g' | sed "s:':\%27:g"`&v=card&s=cname")
         # the following line, found in the line above, is a WIP. Conversion from single quotes to %27 strings in url matching not yet working.
-echo -n $CARDNAME | sed 's: :+:g' | sed "s:':\%27:g"
+echo $CARDNAME | sed 's: :+:g' | sed "s:':\%27:g"
+echo "http://magiccards.info/query?q=`echo -n $CARDNAME | sed 's: :+:g' | sed "s:':\%27:g"`&v=card&s=cname"
+#echo "$MTGI_RAW"
+#echo "$MTGI_RAW" | grep "`echo $CARDNAME_CAP | tr -dc '[:print:]'`"
 
 
         # Chop off heads and tails and ensure we're only working with the html for the proper target card, to eliminate multiple matches and false positives.
         MTGI_RAW_SUB=$(echo $MTGI_RAW | sed "s:^.*>$CARDNAME_CAP</a>.<img::" | sed 's:</table>.*$::')
+
+echo "$MTGI_RAW_SUB"
 
         # Error checking - If we haven't captured our target card, print an error and return fail.
         if echo $MTGI_RAW_SUB | grep -q "$CARDNAME_CAP"; then
